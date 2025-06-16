429d8de2119465b6d4354bb2edf214ec8fa06991
서원빈 <1bins@kakao.com>
2025-06-16 14:16:34 +0900

    :seedling: admin 로그인 페이지

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,7 +1,34 @@
+import style from './admin.module.scss';
+import classnames from 'classnames/bind';
+import { FaUser, FaLock } from "react-icons/fa";
+
+const cx = classnames.bind(style);
+
 export default function AdminPage() {
+
   return(
-    <div>
-      관리자페이지
+    <div className={cx('container')}>
+      <div className={cx('login-form')}>
+        <div className={cx('input-box')}>
+          <FaUser color="#999" />
+          <input
+            type="text"
+            name="userId"
+            placeholder="관리자 ID를 입력하세요"
+            required
+          />
+        </div>
+        <div className={cx('input-box')}>
+          <FaLock color="#999" />
+          <input
+            type="password"
+            name="userPw"
+            placeholder="패스워드를 입력하세요"
+            required
+          />
+        </div>
+        <button type="button">로그인하기</button>
+      </div>
     </div>
   )
 }
