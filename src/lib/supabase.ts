1d9b282083472b9b506d5beeefb44866629e3b53
서원빈 <1bins@kakao.com>
2025-06-18 13:16:21 +0900

    :bulb: supabase 토큰 기능 주석 추가

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,9 @@ import {Database} from "@/lib/database";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  auth: {
+    // persistSession: false,
+    // autoRefreshToken: false // 토큰 리프레쉬 방지
+  }
+});
