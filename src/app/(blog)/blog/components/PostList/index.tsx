8e328b2ace0dc8a999cd94b65a7f56652fbf4e20
서원빈 <1bins@kakao.com>
2025-06-24 14:20:08 +0900

    :sparkles: 포스트 없을 시 노출할 아이템

--- src/app/(blog)/blog/components/PostList/index.tsx
+++ src/app/(blog)/blog/components/PostList/index.tsx
@@ -10,35 +10,42 @@ const cx = classname.bind(style);
 export const PostList = ({ posts }: { posts: PostData[] }) => {
   return(
     <div className={cx('post-list-box')}>
-      <ul>
-        {posts.map(post => (
-          <li
-            key={post.id}
-            className={cx('post-item')}
-          >
-            <article>
-              <Link href={`/blog/post/${post.id}`}>
-                <div className={cx('thumb-box')}>
-                  <Image
-                    width={433}
-                    height={232}
-                    src={post.thumbnailUrl || '/posts/thumbnail-error.jpg'}
-                    alt={`${post.title} 썸네일 이미지`}
-                  />
-                </div>
-                <div className={cx('cont-box')}>
-                  <p className={cx('category')}>{post.category}</p>
-                  <p className={cx('title')}>{post.title}</p>
-                  <p className={cx('date')}>
-                    <MdOutlineCalendarMonth color="#999" />
-                    <span>{new Date(`${post.created_at}`).toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric'})}</span>
-                  </p>
-                </div>
-              </Link>
-            </article>
-          </li>
-        ))}
-      </ul>
+      {
+        posts.length > 0 ?
+          <ul>
+            {posts.map(post => (
+              <li
+                key={post.id}
+                className={cx('post-item')}
+              >
+                <article>
+                  <Link href={`/blog/post/${post.id}`}>
+                    <div className={cx('thumb-box')}>
+                      <Image
+                        width={433}
+                        height={232}
+                        src={post.thumbnailUrl || '/posts/thumbnail-error.jpg'}
+                        alt={`${post.title} 썸네일 이미지`}
+                      />
+                    </div>
+                    <div className={cx('cont-box')}>
+                      <p className={cx('category')}>{post.category}</p>
+                      <p className={cx('title')}>{post.title}</p>
+                      <p className={cx('date')}>
+                        <MdOutlineCalendarMonth color="#999" />
+                        <span>{new Date(`${post.created_at}`).toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric'})}</span>
+                      </p>
+                    </div>
+                  </Link>
+                </article>
+              </li>
+            ))}
+          </ul>
+          :
+          <div className={cx('list-none-box')}>
+            게시글이 없습니다.
+          </div>
+      }
     </div>
   )
 }
