0afe780e90c22129fc8b29414bc44ae29e1146bf
서원빈 <1bins@kakao.com>
2025-06-25 16:36:04 +0900

    :white_check_mark: about 프로젝트 모달 연결
    (cherry picked from commit 693fdb3e893d96e39bc59aef2064507c4ce980f6)

--- src/app/(blog)/about/components/ProjectItem/index.tsx
+++ src/app/(blog)/about/components/ProjectItem/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import style from './projectItem.module.scss';
 import classnames from 'classnames/bind';
 import {Project} from "@/data/about/projects";
@@ -5,17 +7,25 @@ import {SkillList} from "@/app/(blog)/about/components/SkillList";
 
 const cx = classnames.bind(style);
 
+interface Props extends Project{
+  modalOpen: () => void;
+}
+
 export const ProjectItem = ({
   id,
   company,
   title,
   description,
-  skills
-}: Project) => {
+  skills,
+  modalOpen
+}: Props) => {
   const handleItemStyle = id.includes('s') ? 'typeA' : id.includes('c') ? 'typeB' : 'typeC';
 
   return(
-    <div className={cx('project-item', handleItemStyle)}>
+    <div
+      className={cx('project-item', handleItemStyle)}
+      onClick={modalOpen}
+    >
       <p className={cx('company')}>{company}</p>
       <p className={cx('title')}>{title}</p>
       <p className={cx('description')}>{description}</p>
